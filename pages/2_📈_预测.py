3ba3337a74c3dee39e08ae4d0eb81f99c389e157
smysvt1004 <smysvt1004@gmail.com>
2023-06-21 16:34:54 +0800

    修改预测图片同时生成

--- pages/2_📈_预测.py
+++ pages/2_📈_预测.py
@@ -36,43 +36,79 @@ with col2:
     # 创建按钮
     button_clicked = st.button("预测",type="primary")
 
+# # 点击按钮后才会输出预测日期范围
+# if button_clicked:
+#     with col1:
+#         st.write("预测日期范围:", start_date, "到", end_date)
+#         df=predict_df(start_date,end_date,selected_features,selected_fan)
+#         if(len(selected_features)==2):
+#             # 创建一个空的占位符
+#             chart_placeholder = st.empty()
+#             # 设置绘图速度（延迟时间）
+#             delay = 0.02
+#             # 遍历每个数据点，并逐步绘制图表
+#             for i in range(len(df)):
+#                 data_to_plot = df.iloc[:i+1].set_index('DATATIME')
+#                 # 更新占位符内容，绘制图表
+#                 chart_placeholder.line_chart(data_to_plot)
+#                 # 添加延迟，控制绘图速度
+#                 time.sleep(delay)
+#
+#         if(len(selected_features)==3):
+#             # 创建一个空的占位符
+#             chart_placeholder = st.empty()
+#             # 设置绘图速度（延迟时间）
+#             delay = 0.02
+#             # 遍历每个数据点，并逐步绘制图表
+#             for i in range(len(df)):
+#                 data_to_plot = df.iloc[:i+1].set_index('DATATIME')['ROUND(A.POWER,0)']
+#                 # 更新占位符内容，绘制图表
+#                 chart_placeholder.line_chart(data_to_plot)
+#                 # 添加延迟，控制绘图速度
+#                 time.sleep(delay)
+#             chart_placeholder = st.empty()
+#             # 设置绘图速度（延迟时间）
+#             delay = 0.05
+#             # 遍历每个数据点，并逐步绘制图表
+#             for i in range(len(df)):
+#                 data_to_plot = df.iloc[:i+1].set_index('DATATIME')['YD15']
+#                 # 更新占位符内容，绘制图表
+#                 chart_placeholder.line_chart(data_to_plot)
+#                 # 添加延迟，控制绘图速度
+#                 time.sleep(delay)
 # 点击按钮后才会输出预测日期范围
 if button_clicked:
     with col1:
         st.write("预测日期范围:", start_date, "到", end_date)
-        df=predict_df(start_date,end_date,selected_features,selected_fan)
-        if(len(selected_features)==2):
-            # 创建一个空的占位符
-            chart_placeholder = st.empty()
+        df = predict_df(start_date, end_date, selected_features, selected_fan)
+        if len(selected_features) == 2:
+            # 创建两个空的占位符
+            chart_placeholder1 = st.empty()
+            chart_placeholder2 = st.empty()
             # 设置绘图速度（延迟时间）
             delay = 0.02
             # 遍历每个数据点，并逐步绘制图表
             for i in range(len(df)):
-                data_to_plot = df.iloc[:i+1].set_index('DATATIME')
+                data_to_plot_1 = df.iloc[:i + 1].set_index('DATATIME')[selected_features[0]]
+                data_to_plot_2 = df.iloc[:i + 1].set_index('DATATIME')[selected_features[1]]
                 # 更新占位符内容，绘制图表
-                chart_placeholder.line_chart(data_to_plot)
+                chart_placeholder1.line_chart(data_to_plot_1)
+                chart_placeholder2.line_chart(data_to_plot_2)
                 # 添加延迟，控制绘图速度
                 time.sleep(delay)
-    
-        if(len(selected_features)==3):
-            # 创建一个空的占位符
-            chart_placeholder = st.empty()
+
+        if len(selected_features) == 3:
+            # 创建两个空的占位符
+            chart_placeholder1 = st.empty()
+            chart_placeholder2 = st.empty()
             # 设置绘图速度（延迟时间）
             delay = 0.02
             # 遍历每个数据点，并逐步绘制图表
             for i in range(len(df)):
-                data_to_plot = df.iloc[:i+1].set_index('DATATIME')['ROUND(A.POWER,0)']
+                data_to_plot_1 = df.iloc[:i + 1].set_index('DATATIME')['ROUND(A.POWER,0)']
+                data_to_plot_2 = df.iloc[:i + 1].set_index('DATATIME')['YD15']
                 # 更新占位符内容，绘制图表
-                chart_placeholder.line_chart(data_to_plot)
-                # 添加延迟，控制绘图速度
-                time.sleep(delay)
-            chart_placeholder = st.empty()
-            # 设置绘图速度（延迟时间）
-            delay = 0.05
-            # 遍历每个数据点，并逐步绘制图表
-            for i in range(len(df)):
-                data_to_plot = df.iloc[:i+1].set_index('DATATIME')['YD15']
-                # 更新占位符内容，绘制图表
-                chart_placeholder.line_chart(data_to_plot)
+                chart_placeholder1.line_chart(data_to_plot_1)
+                chart_placeholder2.line_chart(data_to_plot_2)
                 # 添加延迟，控制绘图速度
                 time.sleep(delay)
